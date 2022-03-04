3fa41e0e33015d7c38dfdd70ccbfe8212a4ee260
Harsha <kusana.harsha@gmail.com>
2022-03-04 13:08:48 +0530

    added classes to check

---
+++ purse001/src/com/bean/Coin.java
@@ -0,0 +1,8 @@
+package com.bean;
+
+public class Coin {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ purse001/src/com/purse/Purse.java
@@ -0,0 +1,13 @@
+package com.purse;
+
+import java.util.List;
+
+import com.bean.Coin;
+
+public class Purse {
+	private List<Coin> coins;
+	private int count;
+	private int capacity;
+	
+
+}
